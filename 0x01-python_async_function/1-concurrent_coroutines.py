680266b706e58e01c5a4557c080985bc3fb07f70
Sholycul <michaeloluwasholaiji2020@gmail.com>
2024-08-05 13:19:07 -0700

    python async functions

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+""" Module that contains Asynchronous coroutine that takes in two arguments
+  and returns a list of delayed float values.
+
+  Returns:
+    List[float]: A list of delayed float values.
+"""
+
+import asyncio
+from typing import List
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """ Asynchronous coroutine that takes in two arguments
+          and returns a list of delayed float values.
+    """
+
+    delayed_tasks = []  # init empty list to store delayed tasks
+    tasks = [wait_random(max_delay) for _ in range(n)]
+    completed = asyncio.as_completed(tasks)
+    for task in completed:
+        result = await task
+        delayed_tasks.append(result)
+    return delayed_tasks  # list of delayed tasks sorted in ascending order
